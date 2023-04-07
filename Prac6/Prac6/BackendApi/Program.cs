290d74d2754db5a9f90e084a2401940e31bf6f97
Kkkendz <name13no24@gmail.com>
2023-04-07 10:06:16 +0300

    Prac6

---
+++ Prac6/Prac6/BackendApi/Program.cs
@@ -0,0 +1,48 @@
+using Domain.Models;
+using Domain.Interfaces;
+using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ShopsContext>(
+                options => options.UseSqlServer(
+                    "Server=LAB116-P;Database=Shops1;User Id=sa;Password=12345;"));
+
+            builder.Services.AddScoped<Domain.Interfaces.IRepositoryWrapper, RepositoryWrapper>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Prac6/Prac6/Domain/Migrations/20230407064904_MigrationName.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    public partial class MigrationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryType",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    delivery_type = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryType", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    role = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    status = table.Column<bool>(type: "bit", nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: true),
+                    categoryID = table.Column<int>(type: "int", nullable: true),
+                    price = table.Column<decimal>(type: "money", nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Product__categor__31EC6D26",
+                        column: x => x.categoryID,
+                        principalTable: "Category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specification",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    categoryID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specification", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Specifica__categ__2F10007B",
+                        column: x => x.categoryID,
+                        principalTable: "Category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    surname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    login = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    roleID = table.Column<int>(type: "int", nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Users__roleID__2A4B4B5E",
+                        column: x => x.roleID,
+                        principalTable: "Role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductSpecification",
+                columns: table => new
+                {
+                    productID = table.Column<int>(type: "int", nullable: true),
+                    specificationID = table.Column<int>(type: "int", nullable: true),
+                    value = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK__ProductSp__speci__34C8D9D1",
+                        column: x => x.specificationID,
+                        principalTable: "Specification",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK__ProductSp__value__33D4B598",
+                        column: x => x.productID,
+                        principalTable: "Product",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    productID = table.Column<int>(type: "int", nullable: true),
+                    count = table.Column<int>(type: "int", nullable: true),
+                    userID = table.Column<int>(type: "int", nullable: true),
+                    date_cart = table.Column<DateTime>(type: "date", nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Cart__deleted__3F466844",
+                        column: x => x.productID,
+                        principalTable: "Product",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__Cart__userID__403A8C7D",
+                        column: x => x.userID,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    statusID = table.Column<int>(type: "int", nullable: true),
+                    userID = table.Column<int>(type: "int", nullable: true),
+                    date_order = table.Column<DateTime>(type: "date", nullable: true),
+                    delivery_typeID = table.Column<int>(type: "int", nullable: true),
+                    deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Orders__deleted__37A5467C",
+                        column: x => x.userID,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__Orders__delivery__398D8EEE",
+                        column: x => x.delivery_typeID,
+                        principalTable: "DeliveryType",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK__Orders__statusID__38996AB5",
+                        column: x => x.statusID,
+                        principalTable: "Status",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductOrder",
+                columns: table => new
+                {
+                    userID = table.Column<int>(type: "int", nullable: true),
+                    productID = table.Column<int>(type: "int", nullable: true),
+                    value = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK__ProductOr__produ__3C69FB99",
+                        column: x => x.productID,
+                        principalTable: "Product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK__ProductOr__value__3B75D760",
+                        column: x => x.userID,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_productID",
+                table: "Cart",
+                column: "productID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_userID",
+                table: "Cart",
+                column: "userID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_delivery_typeID",
+                table: "Orders",
+                column: "delivery_typeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_statusID",
+                table: "Orders",
+                column: "statusID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_userID",
+                table: "Orders",
+                column: "userID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_categoryID",
+                table: "Product",
+                column: "categoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductOrder_productID",
+                table: "ProductOrder",
+                column: "productID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductOrder_userID",
+                table: "ProductOrder",
+                column: "userID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSpecification_productID",
+                table: "ProductSpecification",
+                column: "productID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSpecification_specificationID",
+                table: "ProductSpecification",
+                column: "specificationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specification_categoryID",
+                table: "Specification",
+                column: "categoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_roleID",
+                table: "Users",
+                column: "roleID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cart");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "ProductOrder");
+
+            migrationBuilder.DropTable(
+                name: "ProductSpecification");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryType");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Specification");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
