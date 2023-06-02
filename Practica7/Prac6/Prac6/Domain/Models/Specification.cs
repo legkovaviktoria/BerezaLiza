1698a8ec59a25ad5eee2072389fca8b441fdef6d
BerezkaVika <ycheb0@mail.ru>
2023-06-02 11:47:59 +0300

    Prac7

---
+++ Practica7/Prac6/Prac6/Domain/Models/Specification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public partial class Specification
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual Category? Category { get; set; }
+    }
+}
